bbcece794ad6465f31f8889d3b355d8ae05a95a1
Shivang Nagaria <shivang.nagaria@gmail.com>
2022-11-05 16:15:25 +0530

    Added simple prompt

--- main.go
+++ main.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"log"
+	"os"
 
 	"github.com/shivtej1505/gosql/command"
 )
@@ -9,9 +12,29 @@ import (
 func main() {
 	fmt.Println("this is main")
 
+	startPrompt()
+
 	// insert 5 values
 	// read them back
-	executeCommands()
+	//executeCommands()
+}
+
+func startPrompt() {
+	//scanner := bufio.NewScanner(os.Stdin)
+	reader := bufio.NewReader(os.Stdin)
+
+	var query string
+	var delim byte
+	var err error
+	delim = ';'
+	for {
+		fmt.Printf("gosql>")
+		query, err = reader.ReadString(delim)
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Printf("%s\n", query)
+	}
 }
 
 func executeCommands() {
